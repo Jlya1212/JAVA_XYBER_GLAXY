903184886239d5a1523b14771c13863efbfccaa2
Kai7314 <kaikaikai7314@gmail.com>
2025-03-21 22:51:02 +0800

    add registerUsername method

--- src/java_assignment/Main.java
+++ src/java_assignment/Main.java
@@ -2,10 +2,18 @@
 package java_assignment;
 import java.util.Scanner;
 public class Main {
-
- 
+int i = 0;
+String selection;
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.print("Hello World!");
+        System.out.print("Login or Register(L/R):");
+        String selection = input.next();
+        while(selection != "R" || selection != "L"){
+        if(selection == "R"){
+
+        }
+        }
     }
     
     public String RegisterUsername(){
