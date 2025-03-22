a3c64ecfc3b5758d14e6764a162712de7095817a
student <student@PC06>
2025-03-22 09:08:46 +0800

    add user class

--- src/java_assignment/Main.java
+++ src/java_assignment/Main.java
@@ -1,26 +1,13 @@
 
 package java_assignment;
-import java.util.Scanner;
-public class Main {
-int i = 0;
-String selection;
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Hello World!");
-        System.out.print("Login or Register(L/R):");
-        String selection = input.next();
-        while(selection != "R" || selection != "L"){
-        if(selection == "R"){
 
-        }
-        }
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    
     }
     
-    public String RegisterUsername(){
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your name");
-        String RegisterUsername = input.next();
-        return RegisterUsername;
-    }
-    //this is a comment
+    
 }
---
+++ src/java_assignment/User.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_assignment;
+
+/**
+ *
+ * @author student
+ */
+public class User {
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+
+    public User(String username, String password, String name, String email){
+    this.username = username;
+    this.password = password;
+    this.name     = name;
+    this.email    = email;
+    }
+}
